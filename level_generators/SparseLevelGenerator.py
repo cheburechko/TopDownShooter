9a768b8d1f73b2e084cd8ea20134b70e0c547bb9
Pavel Cherenkov <cherenkov.p.a@yandex.ru>
2015-09-06 13:32:02 +0600

    Added SparaseLevelGenerator, so far generates only a draft of level geometry

--- level_generators/SparseLevelGenerator.py
+++ level_generators/SparseLevelGenerator.py
@@ -1,7 +1,81 @@
 __author__ = 'thepunchy'
 
-from
+from libs.Vec2D import Vec2d
+from libs.Delaunay2d import Delaunay2d
+import random
+
+from geometry_shortcut import *
 
 class SparseLevelGenerator(object):
     def __init__(self):
-        self.area = Vec2
+        self.area = None
+        self.roomNumber = None
+        self.minRoomSize = None
+        self.points = []
+
+    def generate_random_point(self):
+        while True:
+            p = Vec2d(random.uniform(self.area[0][0], self.area[1][0]),
+                      random.uniform(self.area[0][1], self.area[1][1]))
+            repick = False
+            for point in self.points:
+                if p.get_distance(point) < 2*self.minRoomSize:
+                    repick = True
+                    break
+            if not repick:
+                return p
+
+    def generate(self):
+        self.points = []
+        for i in range(self.roomNumber):
+            self.points += [self.generate_random_point()]
+        delaunay = Delaunay2d(self.points)
+
+        segments = []
+        points = delaunay.points
+        maxSize = [2**32] * len(points)
+        roomSizes = [0] * len(points)
+        edges = {key: list() for key in range(len(points))}
+        for edge in delaunay.getEdges():
+            dropEdge = False
+            # check if edge if far enough from all points
+            for i in range(len(points)):
+                if i in edge:
+                    continue
+                vertex = points[i] - points[edge[0]]
+                base = points[edge[1]] - points[edge[0]]
+                # check if point projects on edge
+                proj = vertex.dot(base) / base.get_length_sqrd()
+                if proj < 0 or proj > 1:
+                    continue
+                # check if edge is to close to the point
+                if (vertex - base*proj).get_length() < self.minRoomSize:
+                    dropEdge = True
+                    break
+
+            if dropEdge:
+                continue
+
+            d = points[edge[0]].get_distance(points[edge[1]])
+            edges[edge[0]] += [edge[1]]
+            edges[edge[1]] += [edge[0]]
+            maxSize[edge[0]] = min(maxSize[edge[0]], d - self.minRoomSize)
+            maxSize[edge[1]] = min(maxSize[edge[1]], d - self.minRoomSize)
+            segments += [Segment(points[edge[0]], end=points[edge[1]])]
+
+        rooms = []
+        for i in range(len(points)):
+            roomSizes[i] = random.uniform(self.minRoomSize, maxSize[i])
+            for j in edges[i]:
+                maxSize[j] = min(maxSize[j],
+                                 points[i].get_distance(points[j]) - roomSizes[i])
+            rooms += [Circle(points[i], 0, roomSizes[i])]
+
+        result = []
+        for segment in segments:
+            if len(segment.get_collisions(rooms)) == 2:
+                result += [segment]
+
+        return result+rooms
+
+
---
+++ test/level_generators/sparse_level_genertor.py
@@ -0,0 +1,38 @@
+__author__ = 'thepunchy'
+
+from level_generators.SparseLevelGenerator import SparseLevelGenerator
+from libs.Vec2D import Vec2d
+import registry, pygame
+from pygame.locals import *
+from graphics.Camera import Camera
+
+SCREEN_AREA = (800, 600)
+BACKGROUND = (0, 128, 0)
+COLOR = (0, 0, 0)
+
+gen = SparseLevelGenerator()
+gen.area = (Vec2d(0, 0), Vec2d(SCREEN_AREA))
+
+screen = pygame.display.set_mode(SCREEN_AREA, DOUBLEBUF)
+screen.fill(BACKGROUND)
+camera = Camera(screen.get_rect(), 0, 1)
+
+gen.minRoomSize = 100
+gen.roomNumber = 10
+
+edges = gen.generate()
+
+
+for shape in edges:
+    shape.draw(screen, camera, COLOR)
+
+pygame.display.flip()
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            run = False
+            break
+    pygame.time.wait(100)
